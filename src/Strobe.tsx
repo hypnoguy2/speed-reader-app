e3ddacd81d0b0e4d35f33ad627a184eb5388ee16
Gero Birkhoelzer <gero.birkhoelzer@uni-konstanz.de>
2021-10-21 09:53:30 +0200

    changes

---
+++ src/Strobe.tsx
@@ -0,0 +1,39 @@
+import React, { CSSProperties, useCallback, useEffect, useMemo, useState } from "react";
+import _ from "lodash";
+
+const styles: CSSProperties = {
+    width: "100%",
+    height: "100%",
+};
+
+export interface StrobeProps {
+    fps?: number;
+    colors?: string[];
+}
+
+const Strobe = (props: StrobeProps) => {
+    const colors = useMemo(() => props.colors || ["white", "black"], [props]);
+    const [bg, setBg] = useState<string>(colors[0]);
+
+    const updateBg = useCallback(() => {
+        setBg(
+            (state) =>
+                colors.filter((c) => c !== state)[_.random(0, colors.length - 1)]
+        );
+    }, [colors]);
+
+    useEffect(() => {
+        const time = props.fps ? (1 / props.fps) * 1000 : 1000;
+        const interval = setInterval(updateBg, time);
+
+        return () => clearInterval(interval);
+    }, [props, updateBg]);
+
+    return (
+        <div className="Strobe" style={{ ...styles, background: bg }}>
+            {bg}
+        </div>
+    );
+};
+
+export default Strobe;
