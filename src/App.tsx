dca74d8dc1e2392f930f0d4ef8ee1d935ba0c655
Hypnoguy <gero.birkhoelzer@uni-konstanz.de>
2021-11-22 15:42:42 +0100

    Squashed commit of the following:
    commit 5fab9c1ac68b8797944983ea230484b75b6f67b6
    Author: Hypnoguy <gero.birkhoelzer@uni-konstanz.de>
    Date:   Tue Nov 16 22:28:16 2021 +0100
    
        build now in docs folder
    
    commit 7a738d71314ed64603284180e69934b403ac191f
    Author: Gero Birkhoelzer <gero.birkhoelzer@uni-konstanz.de>
    Date:   Sat Nov 6 04:08:23 2021 +0100
    
        cleanup in app, this will be released
    
    commit ca7ed0b2bf0f618aff3c7546d0de45466c14ab88
    Author: Gero Birkhoelzer <gero.birkhoelzer@uni-konstanz.de>
    Date:   Sat Nov 6 03:18:14 2021 +0100
    
        clean up to make presentable

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,41 @@
 import { ChangeEvent, useEffect, useState } from "react";
-import { Button, Card, Container, Form, ListGroup, Modal, Nav, Navbar, Tab } from "react-bootstrap";
+import {
+    Button,
+    Card,
+    Container,
+    Form,
+    ListGroup,
+    Modal,
+    Nav,
+    Navbar,
+    Tab,
+    Toast,
+    ToastContainer,
+} from "react-bootstrap";
+
 import { standardWrapper, pivot } from "./Helpers";
 import { HowToPage } from "./HowToPage";
-
 import { useScriptDisplay } from "./ScriptDisplay";
-import { induction } from "./Scripts";
-import Strobe from "./Strobe";
+import { example } from "./Scripts";
 
 const App = () => {
     const [menuOpen, setMenuOpen] = useState(true);
     const [activeKey, setActiveKey] = useState("howTo");
-
-    const [script, setScript] = useState(induction);
-
-    const [fontSize, setFontSize] = useState("10");
+    const [script, setScript] = useState(example);
     const [loops, setLoops] = useState(0);
-
     const [usePivot, setUsePivot] = useState(false);
-    const [strobo, setStrobo] = useState(false);
+    const [paused, setPaused] = useState(false);
 
     const {
         script: hookScript,
+        index,
         element,
         isActive,
-        isRunning,
-        wpm,
         handleStart,
         handlePause,
         handleResume,
-        handleStop,
-        breakFor,
-        setWPM,
         setPivotFunction,
+        resetScript,
         setLoops: setHookLoops,
         setScript: setHookScript,
     } = useScriptDisplay(script);
@@ -46,57 +50,37 @@ const App = () => {
 
     // --- effects for useScript implementation ---
     useEffect(() => {
-        if (!menuOpen)
-            setTimeout(() => {
-                handleStart();
-            }, 1000);
-    }, [handleStart, menuOpen]);
-
-    useEffect(() => {
         setHookLoops(loops);
     }, [loops, setHookLoops]);
 
     useEffect(() => {
         const handleSDown = (ev: KeyboardEvent) => {
-            if (ev.key === "m") {
+            if (ev.key === "Escape") {
                 handlePause();
                 setMenuOpen(true);
             }
             if (ev.key === "p") {
-                if (!isActive) return;
-                if (isRunning) handlePause();
-                else handleResume();
-            }
-            if (ev.key === "s") {
-                handleStop();
-            }
-            if (ev.key === "b") {
-                breakFor(2);
-            }
-            if (ev.key === "w") {
-                setWPM(wpm === 300 ? 100 : 300);
+                if (isActive) {
+                    if (paused) {
+                        handleResume();
+                        setPaused(false);
+                    } else {
+                        handlePause();
+                        setPaused(true);
+                    }
+                }
             }
         };
         document.addEventListener("keydown", handleSDown);
 
         return () => document.removeEventListener("keydown", handleSDown);
-    }, [breakFor, handlePause, handleResume, handleStop, isActive, isRunning, setWPM, wpm]);
+    }, [handlePause, handleResume, isActive, paused]);
 
     // -----------------------------------------------
 
     // Option change handlers
     const handleScriptChange = (ev: ChangeEvent<HTMLInputElement>) => {
         setScript(ev.target.value);
-    };
-
-    const handleWPMChange = (ev: ChangeEvent<HTMLInputElement>) => {
-        if (ev.target.validity.valid) {
-            // setWPM(Number(ev.target.value));
-        }
-    };
-
-    const handleFontSizeChange = (ev: ChangeEvent<HTMLInputElement>) => {
-        if (ev.target.validity.valid) setFontSize(ev.target.value);
     };
 
     const handleLoopsChange = (ev: ChangeEvent<HTMLInputElement>) => {
@@ -108,10 +92,6 @@ const App = () => {
         setPivotFunction(() => (ev.target.checked ? pivot : standardWrapper));
     };
 
-    const handleStroboChange = (ev: ChangeEvent<HTMLInputElement>) => {
-        setStrobo(ev.target.checked);
-    };
-
     const handleNavSelect = (eventKey: string | null, e: React.SyntheticEvent<unknown>) => {
         if (eventKey !== null) setActiveKey(eventKey);
     };
@@ -120,10 +100,24 @@ const App = () => {
         setHookScript(script);
     };
 
+    const handleStartScript = () => {
+        setMenuOpen(false);
+        setPaused(false);
+
+        if (!isActive) {
+            if (index !== 0) resetScript();
+            handleStart();
+        } else handleResume();
+    };
+
     return (
         <Container className="h-100 w-100 p-0" fluid>
             <div style={{ fontSize: "10vw" }}>{isActive && element}</div>
-            {strobo && isActive && <Strobe flashOptions={{ flashFrames: 1, loopFrames: 2 }} />}
+            <ToastContainer className="p-3" position="bottom-center">
+                <Toast show={!menuOpen && paused}>
+                    <Toast.Body>Script is paused</Toast.Body>
+                </Toast>
+            </ToastContainer>
             <Modal
                 show={menuOpen}
                 backdrop={false}
@@ -131,8 +125,7 @@ const App = () => {
                 size="lg"
                 scrollable
                 centered
-                dialogClassName="align-items-stretch"
-                onHide={() => setMenuOpen(false)}>
+                dialogClassName="align-items-stretch">
                 <Navbar bg="light">
                     <Container>
                         <Nav
@@ -163,29 +156,6 @@ const App = () => {
                             <Card className="mb-4 border-0">
                                 <ListGroup>
                                     <ListGroup.Item>
-                                        <Form.Label>WPM (Words Per Second)</Form.Label>
-                                        <Form.Control
-                                            type="number"
-                                            value={wpm}
-                                            onChange={handleWPMChange}
-                                            max={1000}
-                                        />
-                                        <Form.Text>
-                                            The value has to be between 100 and 700.
-                                        </Form.Text>
-                                    </ListGroup.Item>
-                                    <ListGroup.Item>
-                                        <Form.Label htmlFor="font-size-input">Font size</Form.Label>
-                                        <Form.Control
-                                            type="number"
-                                            value={fontSize}
-                                            onChange={handleFontSizeChange}
-                                            min={1}
-                                            step={0.1}
-                                        />
-                                        <Form.Text>Fontsize in vw (viewport width).</Form.Text>
-                                    </ListGroup.Item>
-                                    <ListGroup.Item>
                                         <Form.Label>Number of Loops</Form.Label>
                                         <Form.Control
                                             type="number"
@@ -195,7 +165,7 @@ const App = () => {
                                             min={0}
                                         />
                                         <Form.Text>
-                                            Number of times the script should be repeated. 
+                                            Number of times the script should be repeated.
                                         </Form.Text>
                                     </ListGroup.Item>
                                     <ListGroup.Item>
@@ -206,40 +176,6 @@ const App = () => {
                                             checked={usePivot}
                                             onChange={handlePivotChange}
                                         />
-                                        <ListGroup>
-                                            <ListGroup.Item variant={!usePivot ? "secondary" : ""}>
-                                                <Form.Label>Pivot color</Form.Label>
-                                                <Form.Control
-                                                    disabled={!usePivot}
-                                                    type="color"
-                                                    defaultValue="#de0000"
-                                                />
-                                            </ListGroup.Item>
-                                        </ListGroup>
-                                    </ListGroup.Item>
-                                </ListGroup>
-                            </Card>
-                            <h5>Strobe settings</h5>
-                            <Card>
-                                <ListGroup variant="flush">
-                                    <ListGroup.Item>
-                                        <Form.Check
-                                            type="checkbox"
-                                            id="use-strobo"
-                                            label="Activate strobo"
-                                            checked={strobo}
-                                            onChange={handleStroboChange}
-                                        />
-                                    </ListGroup.Item>
-                                    <ListGroup.Item>
-                                        <Form.Label>FlashFrames</Form.Label>
-                                        <Form.Control type="number" disabled />
-                                        <Form.Text>Amount of white frames.</Form.Text>
-                                    </ListGroup.Item>
-                                    <ListGroup.Item>
-                                        <Form.Label>Frames</Form.Label>
-                                        <Form.Control type="number" disabled />
-                                        <Form.Text>Amount of total frames.</Form.Text>
                                     </ListGroup.Item>
                                 </ListGroup>
                             </Card>
@@ -255,8 +191,8 @@ const App = () => {
                         onClick={handleApplyScript}>
                         {hookScript === script ? "Script processed" : "Process Script"}
                     </Button>
-                    <Button variant="primary" onClick={() => setMenuOpen(false)}>
-                        Start
+                    <Button variant="primary" onClick={handleStartScript}>
+                        {isActive ? "Resume" : "Start"}
                     </Button>
                 </Modal.Footer>
             </Modal>
